a74f3fd3296b2e08c364ebff22485418314c7c55
nzlap <nz@gmail.com>
2026-01-15 20:52:11 +0700

    pointer v2 new book

--- main.c
+++ main.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-void scmp(char *o, char *n);
 int main(int argc, char *argv[]) {
-  char *originalText = "Helllo owrld";
-  char *new;
-  new = malloc(strlen(originalText));
-  scmp(originalText, new);
-  printf("%s", new);
+  float a;
 
-  free(new);
+  printf("%f", a);
+  printf("%f", a);
   return 0;
-}
-
-void scmp(char *o, char *n) {
-  while ((*n++ = *o++) != '\0') {
-    ;
-  }
 }
