bb30258441bbe1aae985f30470a5987a957b0aff
nzlap <nz@gmail.com>
2026-02-28 12:50:45 +0700

    fix xor main.c

--- main.c
+++ main.c
@@ -9,18 +9,18 @@ struct Node {
     uintptr_t xptr;
     struct Node* next;
 };
-
 struct LINKED_LISTED_XOR {
     struct Node* Head;
     struct Node* Tail;
 };
-
-struct LLXOR_STEP {
-    uintptr_t prev;
-    uintptr_t next;
-    struct Node* current;
-} LLXOR_STEP;
-
+struct LLX_STEP_CONTROLLER {
+    uintptr_t prev_xptr;
+    uintptr_t current_xptr;
+    uintptr_t next_xptr;
+    struct Node* prev_node;
+    struct Node* current_node;
+    struct Node* next_node;
+} LLX_STEP_CONTROLLER;
 enum MODES {
     NORMAL,
     REVERSE
@@ -53,7 +53,6 @@ int main()
 
     LLX_display(mylist, NORMAL);
     printf("\n");
-    LLX_display(mylist, REVERSE);
     LLX_KILLS(mylist);
     return 0;
 }
@@ -62,34 +61,35 @@ void LLX_pop(struct LINKED_LISTED_XOR* list)
 {
     if (list != NULL) {
         if (util_check_null_list(list->Head)) {
-            struct LLXOR_STEP controller_step;
-            struct Node* tmp = list->Head;
-            controller_step.current = list->Head;
-            controller_step.prev = 0;
-            controller_step.next = X0R(controller_step.prev, controller_step.current->xptr);
-            controller_step.current = (struct Node*)controller_step.next;
 
-            list->Head = controller_step.current;
+            struct LLX_STEP_CONTROLLER controller;
 
-            printf("%d -- ttest\n", controller_step.current->value);
-            free(tmp);
+            controller.prev_node = list->Head;
+            printf("%d \n", controller.prev_node->value);
+
+            controller.current_xptr = X0R(0, controller.prev_node->xptr);
+            controller.current_node = (struct Node*)controller.current_xptr;
+            printf("%d \n", controller.current_node->value);
+
+            uintptr_t x = X0R((uintptr_t)controller.prev_node, controller.current_node->xptr);
+            controller.next_node = (struct Node*)x;
+            printf("%d \n", controller.next_node->value);
         }
     }
 };
+
 void LLX_push(struct LINKED_LISTED_XOR* list, int value)
 {
     struct Node* new_node = malloc(sizeof(*new_node));
     new_node->value = value;
     if (!util_check_null_list(list->Head)) {
         // init list here !
-        new_node->next = nullptr;
         new_node->xptr = 0;
         list->Head = new_node;
         list->Tail = new_node;
     } else {
         list->Head->xptr = X0R((uintptr_t)new_node, list->Head->xptr);
         new_node->xptr = (uintptr_t)list->Head;
-        new_node->next = list->Head;
         list->Head = new_node;
     }
 }
@@ -141,14 +141,14 @@ void LLX_display(struct LINKED_LISTED_XOR* list, short mode)
 void LLX_LOOP_NEXT_STEP(struct LINKED_LISTED_XOR* list, void (*func)(struct Node* current_list), short mode)
 {
     if (util_check_null_list((mode == NORMAL ? list->Head : list->Tail))) {
-        struct LLXOR_STEP controll_step;
-        controll_step.prev = 0;
-        controll_step.current = (mode == NORMAL) ? list->Head : list->Tail;
-        while (controll_step.current) {
-            controll_step.next = X0R(controll_step.prev, controll_step.current->xptr);
-            controll_step.prev = (uintptr_t)controll_step.current;
-            func(controll_step.current);
-            controll_step.current = (struct Node*)controll_step.next;
+        struct LLX_STEP_CONTROLLER controll;
+        controll.prev_xptr = 0;
+        controll.current_node = (mode == NORMAL) ? list->Head : list->Tail;
+        while (controll.current_node) {
+            controll.next_xptr = X0R(controll.prev_xptr, controll.current_node->xptr);
+            controll.prev_xptr = (uintptr_t)controll.current_node;
+            func(controll.current_node);
+            controll.current_node = (struct Node*)controll.next_xptr;
         }
     }
 }
@@ -161,15 +161,15 @@ struct LINKED_LISTED_XOR* NEW_LIST_XOR()
     return l;
 }
 
-uintptr_t X0R(uintptr_t prev, uintptr_t current)
+uintptr_t X0R(uintptr_t prev_node, uintptr_t current_xptr)
 {
-    return prev ^ current;
+    return prev_node ^ current_xptr;
 }
 
 void util_print_current_value(struct Node* current_list)
 {
     if (util_check_null_list(current_list)) {
-        printf("%d -> ", current_list->value);
+        printf("%p : %d -> ", current_list, current_list->value);
     }
 }
 
