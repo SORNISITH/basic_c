ccefdf66f0e5dc7b57cd78685177c51e446b8ee4
nzlap <nz@gmail.com>
2026-02-28 16:40:37 +0700

    delete some sh s

--- remainder_app/main.c
+++
@@ -1,8 +0,0 @@
-#include "reminder.h"
-#include <stdlib.h>
-
-int main(int argc, char* argv[])
-{
-    reminder_init();
-    return EXIT_SUCCESS;
-}
--- remainder_app/reminder.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define MAX_MSG 60
-#define MAX_REMAINDER 50
-
-int reminder_init();
